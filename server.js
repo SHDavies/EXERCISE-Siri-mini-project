4d68f3864f1d9cbe561dd8b8d0d6c6c32084da5b
Spencer Davies <spencerhdavies@gmail.com>
2015-08-06 13:05:03 -0600

    added security

--- server.js
+++ server.js
@@ -5,13 +5,19 @@ var messages = ["Hello there", "Wat", "I don't understand", "I can help with tha
 var onRequest = function(req, res) {
 	if (req.method === 'GET') {
 		var randomNum = Math.floor(Math.random() * 4);
-		res.writeHead(200, {
-		  'Connection': 'close',
-		  'Content-Type': 'application/json',
-		  'Access-Control-Allow-Origin': '*',
-		  'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-		  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-		});
+		res.statusCode = 200;
+
+		res.setHeader('Content-Type', 'application/json');
+
+		// Allow any website to access this API
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+		res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+		// Don’t allow scripts or iframes execution from domains we don't trust
+		res.setHeader('X-XSS-Protection', '1; mode=block');
+		res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+		res.setHeader('Content-Security-Policy', "default-src 'self' devmountain.github.io");
 		res.end(JSON.stringify({message: messages[randomNum]}));
 	}
 }
